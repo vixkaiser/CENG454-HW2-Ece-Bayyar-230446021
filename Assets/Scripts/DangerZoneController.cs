fcc11767fe2fb6d048d1040db5a5051897e1fb9f
Ece <ecebayyar@gmail.com>
2026-04-03 20:02:16 +0300

    feat: add DangerZoneController and connect it to FlightExamManager

---
+++ Assets/Scripts/DangerZoneController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DangerZoneController : MonoBehaviour
+{
+    [SerializeField] private FlightExamManager examManager;
+    [SerializeField] private float missileDelay = 5f;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //TODO
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        //TODO
+    }
+}
